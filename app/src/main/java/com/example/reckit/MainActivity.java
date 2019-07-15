1bc61eab485cfe4d4aec8a243bc03187a883b4ad
VivekAlhat <vivekalhat14@outlook.com>
2019-07-15 22:37:32 +0530

    Add Camera Viewer & Cloud

--- app/src/main/java/com/example/reckit/MainActivity.java
+++ app/src/main/java/com/example/reckit/MainActivity.java
@@ -1,12 +1,34 @@
 package com.example.reckit;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.reckit.Helper.InternetCheck;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +55,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraView =(cameraView).findViewById(R.id.camera_view);
 
+        btnDetect = (Button) findViewById(R.id.btn_detect);
+        waitingDialog = new SpotsDialog.Builder().setMessage("Processing..")
+                .setCancelable(false).build();
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                waitingDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap,cameraView.getWidth(),cameraView.getHeight(),false);
+                cameraView.stop();
+
+                runDetector(bitmap);
+            }
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
+        btnDetect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.start();
+                cameraView.captureImage();
+            }
+        });
     }
+
+    private void runDetector(Bitmap bitmap) {
+        final FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        new InternetCheck(new InternetCheck.Consumer() {
+            @Override
+            public void accept(boolean internet) {
+                if(internet){
+                    //If connection is ok then use cloud functionality
+                    FirebaseVisionCloudDetectorOptions options = new FirebaseVisionCloudDetectorOptions.Builder().setMaxResults(1).build();
+                    FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance().getVisionCloudLabelDetector(options);
+                    detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionCloudLabel> firebaseVisionCloudLabels) {
+                            processDataResult(firebaseVisionCloudLabels);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Error!",e.getMessage());
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void processDataResult(List<FirebaseVisionCloudLabel> firebaseVisionCloudLabels) {
+        for (FirebaseVisionCloudLabel label : firebaseVisionCloudLabels){
+            Toast.makeText(this,"Result: "+label.getLabel(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
